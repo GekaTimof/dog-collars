ae50786e3cc47fb1c8e37d0680541813093dbfcd
Lena <lenaphilipenko13@gmail.ru>
2024-06-01 00:39:09 +0800

    post community tests

--- logger.py
+++ logger.py
@@ -2,15 +2,25 @@ import logging
 import sys
 from logging.handlers import TimedRotatingFileHandler
 
-FORMATTER_STRING = " \033[94m %(levelname)s (%(asctime)s): %(message)s (Line: %(lineno)d) %(name)s [%(filename)s]"
+FORMATTER_STRING = "%(levelname)s (%(asctime)s): %(message)s (Line: %(lineno)d) %(name)s [%(filename)s]"
 FORMATTER = logging.Formatter(FORMATTER_STRING)
 LOG_FILE = "log_file.log"
+
+class ColoredFormatter(logging.Formatter):
+    COLORS = {'DEBUG': '\033[94m', 'INFO': '\033[92m', 'WARNING': '\033[93m',
+              'ERROR': '\033[91m', 'CRITICAL': '\033[95m'}
+
+    def format(self, record):
+        log_fmt = f"{self.COLORS.get(record.levelname, '')}%(levelname)s (%(asctime)s): %(message)s (Line: %(lineno)d) %(name)s [%(filename)s]"
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
 def get_logger(logger_name):
     logger = logging.getLogger(logger_name)
     logger.setLevel(logging.INFO)
 
     console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setFormatter(FORMATTER)
+    console_handler.setFormatter(ColoredFormatter())
     logger.addHandler(console_handler)
 
     file_handler = TimedRotatingFileHandler(LOG_FILE, when='midnight')
