401a76bc3b4b4dff08a524b84d1cc6ec25724082
Lena <lenaphilipenko13@gmail.ru>
2024-05-31 22:52:56 +0800

    some collars functions

--- tests/collars_test.py
+++ tests/collars_test.py
@@ -1,11 +1,100 @@
 from conftest import client
 
 class TestNewCollar():
-    '''def test_normal_new_collar(self):
-        response = client.post("/collars/new_collar", json={
-                                                              "token": "string",
-                                                              "mac": "123"
-                                                            })
+    # админ добавляет ошейник
+    def test_normal_new_collar(self):
+        response = client.post("/collars/new_collar",
+        json={
+              "token": "token2",
+              "mac": "1234"
+            })
 
+        assert response.status_code == 200
+
+    # юзер не добавляет ошейник
+    def test_user_new_collar(self):
+        response = client.post("/collars/new_collar",
+        json={
+              "token": "token1",
+              "mac": "1234"
+             })
+
+        assert response.status_code == 400
+
+    # добавление существующего активного ошейника
+    def test_old_collar(self):
+        response = client.post("/collars/new_collar",
+        json={
+              "token": "token2",
+              "mac": "7590274"
+             })
+
+        assert response.status_code == 400
+
+    # добавление существующего неактивного ошейника
+    def test_unactive_collar(self):
+        response = client.post("/collars/new_collar",
+        json={
+              "token": "token2",
+              "mac": "379574"
+             })
+        assert response.status_code == 200
+
+    # ошейник не добавляется без токена
+    def test_noone_collar(self):
+        response = client.post("/collars/new_collar",
+        json={
+              "token": "",
+              "mac": "274"
+             })
+
+        assert response.status_code == 400
+
+class TestAddCollar():
+    # привязываем ошейник
+    def test_normal_add_collars(self):
+        response = client.post("/collars/add_collars", json={
+              "token": "token1",
+              "collar_id": "1"
+            })
+        assert response.status_code == 200
+
+    # привязываем неактивный ошейник
+    def test_unactive_add_collars(self):
+        response = client.post("/collars/add_collars", json={
+              "token": "token1",
+              "collar_id": "3"
+            })
+        assert response.status_code == 400
+
+    # привязываем ошейник с неверным токеном
+    def test_nouser_add_collars(self):
+        response = client.post("/collars/add_collars", json={
+              "token": "token0",
+              "collar_id": "1"
+            })
+        assert response.status_code == 400
+
+    # привязываем ошейник который уже привязан
+    def test_already_add_collars(self):
+        response = client.post("/collars/add_collars", json={
+              "token": "token2",
+              "collar_id": "1"
+            })
+        assert response.status_code == 400
+
+    # привязываем несуществующий ошейник
+    def test_nocollar_add_collars(self):
+        response = client.post("/collars/add_collars", json={
+              "token": "token2",
+              "collar_id": "0"
+            })
+        assert response.status_code == 400
+
+'''class TestOwners():
+    # выводим всех владельцев ошейника
+    def test_one_owner(self):
+        response = client.get("/collars/owners")
+
+        print(response.content)
         assert response.status_code == 200'''
-
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ from fastapi.testclient import TestClient
 from src.main import app
 from src.database import BaseDBModel, engine, DBSession
 import src.users.models as user_models
+import src.collars.models as collars_models
 from src.users.crud import create_user
 from argon2 import PasswordHasher
 
@@ -52,6 +53,39 @@ def started_users():
     DBSession.refresh(db_user2)
     DBSession.refresh(db_user_session1)
     DBSession.refresh(db_user_session2)
+
+@pytest.fixture(scope="session", autouse=True)
+def started_collars():
+    test_collar1 = collars_models.Collars(
+        id=1,
+        mac="7590274",
+        is_active=True
+    )
+    test_collar2 = collars_models.Collars(
+        id=2,
+        mac="379574",
+        is_active=False
+    )
+    test_collar3 = collars_models.Collars(
+        id=3,
+        mac="3333",
+        is_active=False
+    )
+
+    db_collar_user1 = collars_models.Owners(
+        user_id=2,
+        collar_id=1
+    )
+    DBSession.add(db_collar_user1)
+    DBSession.commit()
+    DBSession.refresh(db_collar_user1)
+    DBSession.add(test_collar1)
+    DBSession.add(test_collar2)
+    DBSession.add(test_collar3)
+    DBSession.commit()
+    DBSession.refresh(test_collar2)
+    DBSession.refresh(test_collar1)
+    DBSession.refresh(test_collar3)
 
 
 client = TestClient(app)
